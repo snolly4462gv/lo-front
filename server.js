ab1eb5c3346e61553be34efa340c01e1bb2a9dba
snolly <snolly.gv@gmail.com>
2019-05-06 10:25:27 +0300

    heroku deploy

---
+++ server.js
@@ -0,0 +1,18 @@
+const fallback = require('express-history-api-fallback');
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+const root = __dirname + '/dist';
+app.use(express.static(root));
+//app.use(fallback('index.html', { root }))
+
+// Start the app by listening on the default
+// Heroku port
+app.get('*', function(req, res) {
+  res.sendfile('./dist/index.html')
+})
+var port = process.env.PORT || 8000
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
